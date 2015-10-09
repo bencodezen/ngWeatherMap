d0998886e1e785212cef63ed2b247108acc29135
Ben Hong <bencodezen@gmail.com>
2015-10-08 21:48:31 -0400

    Add basic loading animation between routes

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,5 +1,18 @@
 angular.module('OWMApp', ['ngRoute'])
   .value('owmCities', ['New York', 'Dallas', 'Chicago', 'DC'])
+  .run(function($rootScope, $location, $timeout) {
+    $rootScope.$on('$routeChangeError', function() {
+      $location.path("/error");
+    });
+    $rootScope.$on('$routeChangeStart', function() {
+      $rootScope.isLoading = true;
+    });
+    $rootScope.$on('$routeChangeSuccess', function() {
+      $timeout(function() {
+        $rootScope.isLoading = false;
+      }, 1000);
+    });
+  })
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
       templateUrl : 'home.html',
